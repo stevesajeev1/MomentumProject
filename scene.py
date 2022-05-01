3d0e94e143c93d67f7d471544d87313cf928a018
stevesajeev1 <76565057+stevesajeev1@users.noreply.github.com>
2022-04-30 20:13:58 -0400

    Minor typo

--- scene.py
+++ scene.py
@@ -35,7 +35,7 @@ class ImpulseMomentum(Scene):
         definition[41:48].set_color(GREEN)
         derivation = Text("It can be derived from Newton's Second Law of Motion.", font_size=25).next_to(definition, DOWN * 2)
         equation = MathTex(r"\overrightarrow{F}", r"=", r"m", r"\overrightarrow{a}", font_size=50).next_to(derivation, DOWN * 5)
-        acceleration = MathTex(r"\overrightarrow{F}", r"=", r"m", r"\frac{\Delta\pverrightarrow{v}}{\Delta t}", font_size=50).move_to(equation)
+        acceleration = MathTex(r"\overrightarrow{F}", r"=", r"m", r"\frac{\Delta\overrightarrow{v}}{\Delta t}", font_size=50).move_to(equation)
         simplification = MathTex(r"\overrightarrow{F}", r"=", r"\frac{m\Delta\overrightarrow{v}}{\Delta t}", font_size=50).move_to(acceleration)
         momentum = MathTex(r"\overrightarrow{F}", r"=", r"\frac{\Delta\overrightarrow{p}}{\Delta t}", font_size=50).move_to(simplification)
         self.play(AddTextLetterByLetter(definition), run_time=5)
